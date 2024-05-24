18f418aa2a6dd07d8b4d57f51db9eed511069300
nefanov <efanov@phystech.edu>
2024-05-25 01:20:58 +0300

    playing with function bodies extraction

--- src_processing/test.py
+++ src_processing/test.py
@@ -1,4 +1,7 @@
 from preprocessor import *
+from functiondefextractor import core_extractor # ctags required
+
 
 if __name__ == "__main__":
-    check_from_text()
+    out_put = core_extractor.extractor(r"../test_code")
+    print(out_put)
